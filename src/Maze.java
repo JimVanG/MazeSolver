36aeee4a72be5d0e1efb442b2dbea7838aeaa695
JimVanG <jjvg@me.com>
2013-11-17 17:22:13 -0500

    Init Commit

---
+++ src/Maze.java
@@ -0,0 +1,20 @@
+/*
+ * James Van Gaasbeck
+ * PID: J2686979
+ * COP 3503 : Computer Science II
+ * Professor Sean Szumlanski
+ * Assignment 7 : Maze Solver
+ * Due: Sunday, November 17, 11:59pm
+ * 
+ */
+public class Maze {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
